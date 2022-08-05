50e5fdf12108197def594f2f5e9aabe97dc90335
zxy <zxy@disroot.org>
2022-08-05 20:31:21 +0300

    fix impeachment

--- model/parliament.go
+++ model/parliament.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"database/sql"
 	"errors"
 )
 
@@ -9,13 +8,11 @@ func (m *Model) Parliament(g Group, n int) ([]User, error) {
 	tx := m.db.MustBegin()
 	defer tx.Rollback()
 	users := []User{}
-	err := tx.Select(&users, selectEvents, parliamentMemberEvent, g.GID)
-	if err != nil {
+	if err := tx.Select(&users, selectEvents, parliamentMemberEvent, g.GID); err != nil {
 		return nil, err
 	}
 	if len(users) == 0 {
-		err := tx.Select(&users, randomUsers, g.GID, n)
-		if err != nil {
+		if err := tx.Select(&users, randomUsers, g.GID, n); err != nil {
 			return nil, err
 		}
 		for _, u := range users {
@@ -40,27 +37,19 @@ func (m *Model) Impeachment(g Group, u User, threshold int) (votes int, err erro
 	tx := m.db.MustBegin()
 	defer tx.Rollback()
 
-	parliament := []User{}
-	if err := tx.Select(&parliament, selectEvents, parliamentMemberEvent, g.GID); err != nil {
+	var member bool
+	if err := tx.Get(&member, existsUserEventToday, parliamentMemberEvent, g.GID, u.ID); err != nil {
 		return 0, err
 	}
-	e := false
-	for _, p := range parliament {
-		if p.ID == u.ID {
-			e = true
-			break
-		}
-	}
-	if !e {
+	if !member {
 		return 0, ErrNotParliamentMember
 	}
 
-	var test int
-	err = tx.Get(&test, existsUserEventToday, impeachmentEvent, g.GID, u.ID)
-	if !errors.Is(err, sql.ErrNoRows) {
-		if err != nil {
-			return 0, err
-		}
+	var voted bool
+	if err := tx.Get(&voted, existsUserEventToday, impeachmentEvent, g.GID, u.ID); err != nil {
+		return 0, err
+	}
+	if voted {
 		return 0, ErrAlreadyVoted
 	}
 
