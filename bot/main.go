069949405dfd4b93a4a2b4c987a843c40f8b7f76
zxy <zxy@disroot.org>
2023-03-17 13:41:09 +0300

    bot: actions, bot -> main

--- bot/main.go
+++ bot/main.go
@@ -9,7 +9,9 @@ import (
 	"nechego/game"
 	"nechego/handlers"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 
 	tele "gopkg.in/telebot.v3"
@@ -54,6 +56,35 @@ func main() {
 	bot.Start()
 	<-done
 	log.Println("Successful shutdown.")
+}
+
+// worldInitializer returns a function that registers the fishing
+// record announcer.
+func worldInitializer(bot *tele.Bot) func(*game.World) {
+	return func(w *game.World) {
+		w.History.Announce(handlers.RecordAnnouncer(bot, tele.ChatID(w.TGID)))
+	}
+}
+
+// notifyStop gracefully stops the bot after receiving an interrupt
+// signal and sends an empty structure on the done channel.
+func notifyStop(bot *tele.Bot, universe *game.Universe) (done chan struct{}) {
+	done = make(chan struct{})
+	go func() {
+		interrupt := make(chan os.Signal, 1)
+		signal.Notify(interrupt, syscall.SIGINT, syscall.SIGTERM)
+		<-interrupt
+
+		log.Println("Stopping the bot...")
+		bot.Stop()
+
+		log.Println("Saving the universe...")
+		if err := universe.SaveAll(); err != nil {
+			log.Fatal(err)
+		}
+		done <- struct{}{}
+	}()
+	return done
 }
 
 type Router struct {
