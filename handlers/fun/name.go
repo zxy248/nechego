1465eebf6ae1c083643c9a7cf40a3bd49876e85c
zxy <zxy@disroot.org>
2023-11-30 13:11:44 +0300

    refactor Name, CheckName handlers

--- handlers/fun/name.go
+++ handlers/fun/name.go
@@ -4,7 +4,6 @@ import (
 	"html"
 	"nechego/format"
 	"nechego/handlers"
-	"nechego/handlers/parse"
 	tu "nechego/teleutil"
 	"unicode/utf8"
 
@@ -13,9 +12,10 @@ import (
 
 type Name struct{}
 
-func (h *Name) Match(s string) bool {
-	_, ok := parseName(s)
-	return ok
+var nameRe = handlers.Regexp("^!имя (.+)")
+
+func (h *Name) Match(c tele.Context) bool {
+	return nameRe.MatchString(c.Text())
 }
 
 func (h *Name) Handle(c tele.Context) error {
@@ -23,50 +23,44 @@ func (h *Name) Handle(c tele.Context) error {
 	if !ok {
 		u = c.Sender()
 	}
-	name, _ := parseName(c.Message().Text)
-	if longName(name) {
-		return c.Send(format.LongName(maxNameLength), tele.ModeHTML)
+	name := parseName(c.Text())
+	if !validNameLength(name) {
+		return c.Send(format.LongName(maxNameLength))
 	}
-	if err := authorizeName(c, u); err != nil {
+	if err := promoteUser(c, u); err != nil {
 		return err
 	}
 	if err := setName(c, u, name); err != nil {
-		return c.Send(format.CannotSetName, tele.ModeHTML)
+		return c.Send(format.CannotSetName)
 	}
 	return c.Send(format.NameSet(name), tele.ModeHTML)
 }
 
-func parseName(s string) (name string, ok bool) {
-	if !parse.Seq(
-		parse.Match("!имя"),
-		parse.Str(parse.Assign(&name)),
-	)(s) {
-		return "", false
-	}
-	return html.EscapeString(name), true
+func parseName(s string) string {
+	return html.EscapeString(nameRe.FindStringSubmatch(s)[1])
 }
 
 const maxNameLength = 16
 
-func longName(n string) bool {
-	return utf8.RuneCountInString(n) > maxNameLength
+func validNameLength(s string) bool {
+	return utf8.RuneCountInString(s) <= maxNameLength
 }
 
-func authorizeName(c tele.Context, u *tele.User) error {
+func promoteUser(c tele.Context, u *tele.User) error {
 	return tu.Promote(c, tu.Member(c, u))
 }
 
-func setName(c tele.Context, u *tele.User, n string) error {
-	return c.Bot().SetAdminTitle(c.Chat(), u, n)
+func setName(c tele.Context, u *tele.User, name string) error {
+	return c.Bot().SetAdminTitle(c.Chat(), u, name)
 }
 
 type CheckName struct{}
 
-func (h *CheckName) Match(s string) bool {
-	return handlers.HasPrefix(s, "!имя")
+func (h *CheckName) Match(c tele.Context) bool {
+	return handlers.HasPrefix(c.Text(), "!имя")
 }
 
 func (h *CheckName) Handle(c tele.Context) error {
-	name := tu.Link(c, c.Sender())
-	return c.Send(format.YourName(name), tele.ModeHTML)
+	l := tu.Link(c, c.Sender())
+	return c.Send(format.YourName(l), tele.ModeHTML)
 }
