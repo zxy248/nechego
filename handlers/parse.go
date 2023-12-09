fc5d2f7e8653ae3d91c38a172a6807c3b8d720b0
zxy <zxy@disroot.org>
2023-12-10 02:01:01 +0300

    remove parse package

--- handlers/parse.go
+++ handlers/parse.go
@@ -1,37 +1,10 @@
 package handlers
 
 import (
-	"nechego/handlers/parse"
 	"regexp"
 	"strconv"
 	"strings"
 )
-
-func numCommand(prefix parse.G, s string) (keys []int, ok bool) {
-	ok = parse.Seq(
-		prefix,
-		parse.All(parse.Or(
-			parse.Int(func(n int) {
-				keys = append(keys, n)
-			}),
-			parse.Interval(func(min, max int) {
-				const lim = 20
-				if max-min > lim {
-					max = min + lim
-				}
-				for i := min; i <= max; i++ {
-					keys = append(keys, i)
-				}
-			}),
-		)),
-	)(s)
-	return
-}
-
-func textCommand(prefix parse.G, s string) (text string, ok bool) {
-	ok = parse.Seq(prefix, parse.Str(parse.Assign(&text)))(s)
-	return
-}
 
 func Regexp(pattern string) *regexp.Regexp {
 	return regexp.MustCompile("(?i)" + pattern)
